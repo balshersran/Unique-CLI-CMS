cd5419044339111b8e5252050e3cc53f4571e214
Balsher Sran <balshersran@Balshers-MacBook-Pro.local>
2024-10-11 19:53:59 -0700

    worked out the kinks in my function codes to get my cli to run properly

--- src/cli.ts
+++ src/cli.ts
@@ -1,6 +1,8 @@
 import { pool, connectToDb } from './connections.js';
 import inquirer from 'inquirer';
+import dotenv from 'dotenv';
 
+dotenv.config();
 
 // TODO: wait to connect to database first
 await connectToDb();
@@ -28,86 +30,78 @@ function startCli() {
         ])
         // TODO: take the responses from the choices above and see if they need another function to be called or if they can just update from within
         .then((res) => {
-            if (res.action === 'View All Employees') {
-                const viewAllEmp = `SELECT * FROM employee`;
-                pool.query(viewAllEmp);
-                console.table(viewAllEmp);
-            } else if (res.action === 'Add Employee') {
-                addEmployee();
-                return;
-            } else if (res.action === 'Update Employee Role') {
-
-            } else if (res.action === 'View All Roles') {
-                const viewRoles = `SELECT * FROM TABLE roles`;
-                pool.query(viewRoles);
-                console.table(viewRoles);
-            } else if (res.action === 'Add Role') {
-
-            } else if (res.action === 'View All Departments') {
-                const viewDepartments = `SELECT * FROM TABLE department`;
-                pool.query(viewDepartments);
-                console.table(viewDepartments);
-            } else if (res.action === 'Add Department') {
-                addDepartment();
-            } else {
-                process.exit(0);
+            switch (res.action) {
+                case 'View All Employees':
+                    viewAllEmployees();
+                    startCli();
+                    break;
+                case 'Add Employee':
+                    addEmployee();
+                    break;
+                case 'Update Employee Role':
+                    updateEmployeeRole();
+                    break;
+                case 'View All Roles':
+                    viewAllRoles();
+                    break;
+                case 'Add Role':
+                    addRole();
+                    break;
+                case 'View All Departments':
+                    viewAllDepartments();
+                    break;
+                case 'Add Department':
+                    addDepartment();
+                    break;
+                case 'Exit':
+                    process.exit(7);
+                    break;
+                default:
+                    console.log('Invalid choice. Please try again.');
             }
         })
 }
 startCli();
 
-function addEmployee() {
-    inquirer
-        .prompt([{
-            name: 'first_name',
-            type: 'input',
-            message: 'What is the first name of new Employee',
-        },
-        {
-            name: 'last_name',
-            type: 'input',
-            message: 'What is the last name of the new Employee',
-        }, 
-        {
-            name: 'emp_id',
-            type: 'input',
-            message: 'Please assign an id number',
-        },
-        ])
-        .then((res) => {
-            const firstName = res.first_name;
-            const lastName = res.last_name;
-            const empId = res.emp_id;
-            const empArr = [firstName, lastName, empId];
-            const addEmpSql = `INSERT INTO employee (first_name, last_name, id) VALUES ($1 ,$2 , $3)`;
-            pool.query(addEmpSql, empArr);
-            console.table(addEmpSql);
-        })
-}
-// TODO: updating employee role is changing id to see if employee is now "promoted"
-// function updateEmployeeRole(){}
-
-// function addRole(){}
-// TODO: addDepartment method that will insert a new department into department table, need properties name, id
-function addDepartment(){
-    inquirer
-        .prompt([{
-            name: 'new_dept',
-            type: 'input',
-            message: 'What is the name of the new Department',
-        },
-        {
-            name: 'new_dept_id',
-            type: 'input',
-            message: 'Give your new Department an id number',
-        },
-    ])
-    .then((res) => {
-        const newDept = res.new_dept;
-        const newDeptId = res.new_dept_id;
-        const deptArr = [newDept, newDeptId];
-        const newDeptSql = `INSERT INTO department (name , id) VALUES ($1 , $2)`;
-        pool.query(newDeptSql, deptArr);
-        console.table(newDeptSql);
+function viewAllEmployees() {
+    const sql = `SELECT * FROM employee`;
+    pool.query(sql, (err, res) => {
+        if (err) {
+            console.error('Error:', err)
+        } else {
+            console.table(res.rows)
+        }
     })
 }
+
+function addEmployee() {}
+
+function updateEmployeeRole() {}
+
+function viewAllRoles() {
+    const sql = `SELECT * FROM role`;
+    pool.query(sql, (err, res) => {
+        if(err) {
+            console.error('Error: ', err);
+        } else {
+            console.table(res.rows)
+        }
+    })
+}
+
+function addRole() {}
+
+function viewAllDepartments() {
+    const sql = `SELECT * FROM department`;
+    pool.query(sql, (err, res) => {
+        if(err) {
+            console.error('Error: ',err);
+        } else {
+            console.table(res.rows);
+        }
+    })
+}
+
+function addDepartment() {}
+
+
